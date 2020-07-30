e9c85c0fb65fe535882f7ce3d3afe634a62301c0
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-30 11:59:01 -0700

    Updates

---
+++ precache-manifest.859f5ae6f331392f33f5847df21c9b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce7689efd012261c94141eb437000aa",
+    "url": "/rentalcar/index.html"
+  },
+  {
+    "revision": "b22dbb4b33fba89d49aa",
+    "url": "/rentalcar/static/css/main.2e11ad53.chunk.css"
+  },
+  {
+    "revision": "99cd59adce6885179d38",
+    "url": "/rentalcar/static/js/2.202ee26b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rentalcar/static/js/2.202ee26b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22dbb4b33fba89d49aa",
+    "url": "/rentalcar/static/js/main.7fb67561.chunk.js"
+  },
+  {
+    "revision": "d7890d10a18abaa236dc",
+    "url": "/rentalcar/static/js/runtime-main.bde2cb38.js"
+  }
+]);
